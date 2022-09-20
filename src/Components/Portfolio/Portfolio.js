e0c8e37a39d9db84ebb375ea3eee6d2c86834bde
unknown <soliuabdulkadir28@gmail.com>
2022-09-20 12:43:04 -0700

    view port responsiveness was completed

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -15,43 +15,43 @@ const data=[
     id:1,
     image: IMG1,
     title:'title one',
-    github: 'https://github',
-    demo: 'site name'
+    github: 'https://github.com/Quadri28/my-portfolio',
+    demo: 'devquadriportfolio.netlify.app'
   },
   {
     id:2,
     image: IMG2,
     title:'title one',
-    github: 'https://github',
-    demo: 'site name'
+    github: 'https://githubhttps://github.com/Quadri28/my-shop',
+    demo: 'shopryte.netlify.app'
   },
   {
     id:3,
     image: IMG3,
     title:'title one',
-    github: 'https://github',
-    demo: 'site name'
+    github: 'https://github.com/Quadri28/Recipe-App',
+    demo: 'recipedictionary.netlify.app'
   },
   {
     id:4,
     image: IMG4,
     title:'title one',
-    github: 'https://github',
-    demo: 'site name'
+    github: 'https://github.com/sheriffjimoh/Justin_portfolio',
+    demo: 'https://justin-resume.netlify.app'
   },
   {
     id:5,
     image: IMG5,
     title:'title 5',
-    github: 'https://github',
-    demo: 'site name'
+    github: 'https://github.com/Quadri28/react-tour-website',
+    demo: 'tourtheplain.netlify.app'
   },
   {
     id:6,
     image: IMG6,
     title:'title six',
-    github: 'https://github',
-    demo: 'site name'
+    github: 'https://github.com/Quadri28/maidinaddis.com',
+    demo: 'https://maidinaddisababa.netlify.app/'
   },
 ]
 
