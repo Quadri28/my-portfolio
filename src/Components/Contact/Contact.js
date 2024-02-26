4484dd1a8d03cd91b32b23309382f436ee68c600
Abdulkadir Soliu <abdulkadir.soliu@cwg-plc.com>
2024-02-26 10:26:52 +0100

    changed the mailing system

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,52 +1,90 @@
-import React,{useRef} from 'react'
-import './Contact.css';
-import {MdOutlineEmail} from 'react-icons/md';
-import {RiMessengerLine} from 'react-icons/ri';
-import {BsWhatsapp} from 'react-icons/bs'
-import emailjs from '@emailjs/browser'
+import React, { useState } from "react";
+import "./Contact.css";
+import { MdOutlineEmail } from "react-icons/md";
+import { RiMessengerLine } from "react-icons/ri";
+import { BsWhatsapp } from "react-icons/bs";
 
 const Contact = () => {
-  const form = useRef();
-  const sendEmail = (e)=>{
-    e.preventDefault();
+  const [contact, setContact] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
 
-    emailjs.sendForm('service_0sz9qqg', 'template_0v7a8md', form.current, 'AOJsTx9dCzmrEY5pf')
-      e.target.reset()
-  }
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setContact({ ...contact, [name]: value });
+  };
+
   return (
-    <section id='contact'>
+    <section id="contact">
       <h5>Get in touch</h5>
       <h2>Contact Me</h2>
-      <div className='container contact__container'>
-        <div className='contact__options'>
-            <article className='contact__option'>
-              <MdOutlineEmail className='contact__option-icon'/>
-              <h4>Email</h4>
-              <h5>soliuabdulkadir28@gmail.com</h5>
-              <a href='mailto:soliuabdulkadir28@gmail.com' target='blank'>Send a message</a>
-            </article>
-            <article className='contact__option'>
-              <RiMessengerLine className='contact__option-icon'/>
-              <h4>Messenger</h4>
-              <h5>Quadri Abiola</h5>
-              <a href='https://m.me/?_rdc=1&_rdr' target='blank'>Send a message</a>
-            </article>
-            <article className='contact__option'>
-              <BsWhatsapp contact__option-icon/>
-              <h4>Whatsapp</h4>
-              <h5>+2348131215178</h5>
-              <a href='https://api.whatsapp.com/send?phone=2348131215178' target='blank'>Send a message</a>
-            </article>
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineEmail className="contact__option-icon" />
+            <h4>Email</h4>
+            <h5>soliuabdulkadir28@gmail.com</h5>
+            <a href="mailto:soliuabdulkadir28@gmail.com" target="blank">
+              Send a message
+            </a>
+          </article>
+          <article className="contact__option">
+            <RiMessengerLine className="contact__option-icon" />
+            <h4>Messenger</h4>
+            <h5>Quadri Abiola</h5>
+            <a href="https://m.me/?_rdc=1&_rdr" target="blank">
+              Send a message
+            </a>
+          </article>
+          <article className="contact__option">
+            <BsWhatsapp contact__option-icon />
+            <h4>Whatsapp</h4>
+            <h5>+2348131215178</h5>
+            <a
+              href="https://api.whatsapp.com/send?phone=2348131215178"
+              target="blank"
+            >
+              Send a message
+            </a>
+          </article>
         </div>
-        <form ref={form} onSubmit={sendEmail}>
-          <input type='text' name='name' placeholder='Your full name' className='form-inputs'/>
-          <input type='email' name='email' placeholder='Your Email' className='form-inputs'/>
-          <textarea name='message' rows='7' required placeholder='Your message' className='form-inputs'></textarea>
-          <button type='submit' className='btn bnt-primary'>Send Message</button>
+        <form>
+          <input
+            type="text"
+            name="name"
+            placeholder="Your full name"
+            onChange={handleChange}
+            className="form-inputs"
+          />
+          <input
+            type="email"
+            name="email"
+            placeholder="Your Email"
+            onChange={handleChange}
+            className="form-inputs"
+          />
+          <textarea
+            name="message"
+            rows="7"
+            required
+            placeholder="Your message"
+            onChange={handleChange}
+            className="form-inputs"
+          ></textarea>
+          <a
+            type="submit"
+            className="btn bnt-primary"
+            href={`mailto:soliuabdulkadir28@gmail.com?subject=Client reach-out&body=Hello AbdulQuadri %0A%0D I am ${contact.name} with email: ${contact.email}. %0A%0D ${contact.message}`}
+          >
+            Send Message
+          </a>
         </form>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
